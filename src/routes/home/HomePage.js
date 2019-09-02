a3dbc5c250b9c303cecc6495d4cdb30b763abc44
Anusha <cdan.dharmasena@gmail.com>
2019-09-03 08:33:03 +1000

    Manage account stub

--- src/routes/home/HomePage.js
+++ src/routes/home/HomePage.js
@@ -3,6 +3,7 @@ import * as React from 'react';
 import { Switch, Redirect, Route } from 'react-router-dom';
 import { ExpensesList } from './expenses/ExpensesList';
 import { CreateExpense } from './expenses/CreateExpense';
+import { ManageAccount } from './manageAccount/ManageAccount';
 import { ROUTE } from '../../configs/route';
 
 export function HomePage() {
@@ -13,6 +14,7 @@ export function HomePage() {
       <Switch>
         <Route exact path={ROUTE.CREATE_EXPENSE} component={CreateExpense} />
         <Route exact path={ROUTE.EXPENSES} component={ExpensesList} />
+        <Route exact path={ROUTE.MANAGE_ACCOUNT} component={ManageAccount} />
 
         <Redirect to={ROUTE.EXPENSES} />
       </Switch>
---
+++ src/routes/home/manageAccount/ManageAccount.js
@@ -0,0 +1,7 @@
+// @flow
+import React from 'react';
+import { Link } from '../../../components/Link';
+
+export function ManageAccount() {
+  return <p>Manage Account</p>;
+}
